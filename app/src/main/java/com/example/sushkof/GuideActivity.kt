a3e7a814e8fd53b17c6242e73078a71646a6d46b
asus <pineapple6934@gmail.com>
2021-11-24 01:16:17 +0500

    guide forward button

--- app/src/main/java/com/example/sushkof/GuideActivity.kt
+++ app/src/main/java/com/example/sushkof/GuideActivity.kt
@@ -1,8 +1,13 @@
 package com.example.sushkof
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.example.sushkof.ui.guide.GuideAdapter
@@ -13,6 +18,7 @@ import com.google.android.material.tabs.TabLayoutMediator
 class GuideActivity : AppCompatActivity() {
     lateinit var pager: ViewPager2
     lateinit var tab: TabLayout
+    lateinit var btn_next: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +28,12 @@ class GuideActivity : AppCompatActivity() {
         pager = findViewById(R.id.guide_vp)
         pager.adapter = GuideAdapter(this, Guides().list)
         TabLayoutMediator(tab, pager){_,_->}.attach()
+
+        btn_next = findViewById(R.id.button_next)
+        btn_next.setOnClickListener {
+            if (pager.currentItem != 2) pager.currentItem += 1
+            else SkipClick(btn_next)
+        }
     }
 
     fun SkipClick(view: android.view.View) {
